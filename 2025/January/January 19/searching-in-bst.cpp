9a121c50656c2247d1928489cb3be0298a1605a3
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-01-19 10:49:08 +0600

    10:49

--- 2025/January/January 19/searching-in-bst.cpp
+++ 2025/January/January 19/searching-in-bst.cpp
@@ -13,6 +13,25 @@ class Node{
     }
 };
 
+Node* input_binary_tree(){
+
+    int root_number ; cin >> root_number;
+
+    queue<Node*>q;
+
+    Node *root ;
+
+    if(root_number == -1) root = NULL;
+    else root = new Node(root_number);
+
+    if(root == NULL)return root;
+    else q.push(root);
+
+    
+
+    
+}
+
 
 
 
