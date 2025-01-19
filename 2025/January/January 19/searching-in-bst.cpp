db0108e695c2f7399be32dc5abb304b3b8a2b3db
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-01-19 10:40:35 +0600

    10:40 update

--- 2025/January/January 19/searching-in-bst.cpp
+++ 2025/January/January 19/searching-in-bst.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
 
 
 
