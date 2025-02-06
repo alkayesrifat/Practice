dacd4f2c6d3282e61114f045d4d3585ddbe2e9bf
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-02-06 11:31:29 +0600

    Implement DFS algorithm in pp2.cpp and update input/output files

--- 2025/February/February 6/pp2.cpp
+++ 2025/February/February 6/pp2.cpp
@@ -1,10 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+vector<int> adj_list[1000];
+bool visited_or_not[1000];
+int countt = 0;
 
-int main(){
+void dfs(int src){
 
-    cout << "hello world";
+    visited_or_not[src] = true;
 
+    for(int x : adj_list[src]){
+        if(visited_or_not[x] == false){
+            dfs(x);
+            
+        }
+    }
+
+}
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(visited_or_not,false,sizeof(visited_or_not));
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited_or_not[i] == false)
+        {
+            dfs(i);
+            countt++;
+            
+        }
+        
+    }
     
+    
+    
+
+    cout << countt ;
+
     return 0;
 }
