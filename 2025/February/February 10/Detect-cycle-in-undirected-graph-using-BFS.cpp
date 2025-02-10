084e2eb701b0e98d57a9f192af5cfde4caaf1c9a
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-02-10 19:27:04 +0600

    Add BFS cycle detection implementation and input/output files

---
+++ 2025/February/February 10/Detect-cycle-in-undirected-graph-using-BFS.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[100];
+bool visited[100];
+bool cycle;
+int parenta[100];
+void bfs(int src)
+{
+
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+
+    while (q.empty() == false)
+    {
+        int parent = q.front();
+        q.pop();
+
+        for (auto x : adj_list[parent])
+        {
+            if (visited[x] && parenta[parent] != x)
+            {
+                cycle = true;
+            }
+
+            if (visited[x] == false)
+            {
+                q.push(x);
+                visited[x] = true;
+                parenta[x] = parent;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+    memset(visited, false, sizeof(visited));
+    memset(parenta,-1,sizeof(parenta));
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    cycle = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == false)
+        {
+            bfs(i);
+        }
+    }
+    if (cycle )
+    {
+        cout << "cycle dicatedted";
+    }
+    else
+    {
+        cout << " NO cycle , i love you , etai bastob , hahahahha, ";
+    }
+
+    return 0;
+}
--- 2025/February/February 5/dfs-on-2d-grid-implementation-ii.cpp
+++ 2025/February/February 5/dfs-on-2d-grid-implementation-ii.cpp
@@ -1,58 +1,53 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 char arr[100][100];
 bool visited[100][100];
-vector<pair<int,int>>val = {{-1,0},{1,0},{0,-1},{0,1}};
-int n , m;
+vector<pair<int, int>> val = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
 
-bool valid_or_not(int i , int j){
-    if (i < 0 || i >=n || j < 0 || j >= m)
+bool valid_or_not(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
     {
         return false;
     }
     return true;
-    
-
 }
 
+void dfs(int sri, int srj)
+{
 
-void dfs(int sri,int srj){
 
-    //printing the sri and srj
-    cout << sri << " " << srj << endl ;
+    cout << sri << " " << srj << endl;
     visited[sri][srj] = true;
     for (int i = 0; i < 4; i++)
     {
-        int childi , childj;
+        int childi, childj;
         childi = sri + val[i].first;
         childj = srj + val[i].second;
-        if(valid_or_not(childi,childj) && !visited[childi][childj]){
-            dfs(childi,childj);
+        if (valid_or_not(childi, childj) && !visited[childi][childj])
+        {
+            dfs(childi, childj);
         }
     }
-    
-
-
 }
 
-int main(){
-
+int main()
+{
 
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0;  j< m; j++)
+        for (int j = 0; j < m; j++)
         {
             cin >> arr[i][j];
         }
-        
     }
-    memset(visited,false,sizeof(visited));
-    int sri , srj ; 
+    memset(visited, false, sizeof(visited));
+    int sri, srj;
     cin >> sri >> srj;
 
-    dfs(sri,srj);
-    
+    dfs(sri, srj);
 
     return 0;
 }
