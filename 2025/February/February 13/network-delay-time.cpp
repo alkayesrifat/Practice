b7a03985ebf7b10bb201235eca3709cebb74bee5
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-02-13 20:08:55 +0600

    Add Dijkstra's algorithm implementation for network delay time problem

---
+++ 2025/February/February 13/network-delay-time.cpp
@@ -0,0 +1,57 @@
+// link :::      https://leetcode.com/problems/network-delay-time/description/
+
+class Solution {
+    public:
+        vector<vector<pair<int, int>>> adj;
+        vector<int>dis;
+        void dijkstraa(int src){
+            
+            priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+            pq.push({0,src});
+            dis[src] = 0;
+            while(pq.empty() == false){
+                pair<int, int> parent = pq.top();
+                pq.pop();
+                for(auto child : adj[parent.second]){
+                    if(parent.first + child.second < dis[child.first]){
+                        dis[child.first] = parent.first + child.second;
+                        pq.push({dis[child.first],child.first});
+                    }
+                }
+            }
+            
+        }
+        
+        int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+            adj.resize(n + 1); 
+            
+           
+            dis.resize(n + 1, INT_MAX);
+    
+            for(int i = 0 ; i <times.size(); i++){
+                int a,b,c;
+              
+    
+                a = times[i][0];
+                b = times[i][1];
+                c = times[i][2];
+                    
+           
+                adj[a].push_back({b,c});
+                
+            }
+    
+            dijkstraa(k);
+            
+            int maxTime = 0;
+            for (int i = 1; i <= n; i++) {
+              
+                if (dis[i] == INT_MAX) return -1;
+                
+                maxTime = max(maxTime, dis[i]);
+            }
+            return maxTime;
+    
+            
+        }
+    };
