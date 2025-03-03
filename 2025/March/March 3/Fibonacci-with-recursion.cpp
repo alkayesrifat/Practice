66169a82149582d7b99e6728550b7e63b1dae049
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-03-03 09:34:42 +0600

    Add Fibonacci implementation with multiple recursive approaches and update output file

---
+++ 2025/March/March 3/Fibonacci-with-recursion.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Type one ->
+
+int fib(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int fi = fib(n - 1) + fib(n - 2);
+    return fi;
+}
+
+*/
+/*
+Type Two ->
+
+int fib(int n)
+{
+    if (n == 1 || n ==0)
+    {
+        return n;
+    }
+    
+
+    int fi = fib(n - 1) + fib(n - 2);
+    return fi;
+}
+*/
+
+// type 3->
+int fib(int n)
+{
+    if (n < 2 )
+    {
+        return n;
+    }
+    
+
+    int fi = fib(n - 1) + fib(n - 2);
+    return fi;
+}
+
+int main()
+{
+
+    cout << fib(6);
+    return 0;
+}
