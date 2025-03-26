7f4f6b06c91353d1a367e606b8cc4a000c2289b6
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-03-26 10:57:19 +0600

    a biscuit generator added

---
+++ Problem Solvers Club -- Phitron/Week-1/March 26/A-Biscuit_Generator.cpp
@@ -0,0 +1,22 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-26   10:34:43
+ * ---------------------
+ * Problem Link: https://atcoder.jp/contests/abc125/tasks/abc125_a?lang=en
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a , b ,t;
+    cin >> a >> b >>t ;
+
+    int osama = t / a;
+    cout << osama * b;
+
+    return 0;
+}
---
+++ Problem Solvers Club -- Phitron/Week-1/March 26/B-Not_Found.cpp
@@ -0,0 +1,17 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-26   10:56:57
+ * ---------------------
+ * Problem Link: https://atcoder.jp/contests/abc071/tasks/abc071_b?lang=en
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    
+    return 0;
+}
