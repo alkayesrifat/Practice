062b91552955e36c3822e049ea2437a69f9a2fbc
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-03-26 12:20:08 +0600

    b minor change added

---
+++ Problem Solvers Club -- Phitron/Week-1/March 26/B-Minor_Change.cpp
@@ -0,0 +1,36 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-26   12:16:53
+ * ---------------------
+ * Problem Link: https://atcoder.jp/contests/abc172/tasks/abc172_b?lang=en
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    string s;
+    string t;
+
+    cin >> s ;
+    cin  >> t;
+
+    int count = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != t[i])
+        {
+            count ++;
+        }
+        
+    }
+    cout << count;
+
+    
+    return 0;
+}
