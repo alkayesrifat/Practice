ee1e9e21dd4c629b6e2f86cb6c88c87377fd3a60
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2025-03-26 12:47:28 +0600

    a plus or minus added

---
+++ Problem Solvers Club -- Phitron/Week-1/March 26/A.Plus-or-Minus.cpp
@@ -0,0 +1,36 @@
+/**
+ * All Praise to Allah
+ * ---------------------
+ * Author: Al Kayes Rifat
+ * Created: 2025-03-26   12:41:04
+ * ---------------------
+ * Problem Link: https://codeforces.com/problemset/problem/1807/A
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n ; cin >> n ;
+    
+    while (n--)
+    {
+        int a , b , c;
+        cin >> a >> b >>c ;
+        if (a + b == c )
+        {
+            cout << "+" << endl;
+        }
+        else
+        {
+            cout << "-" << endl;
+        }
+        
+        
+    }
+    
+
+    
+    return 0;
+}
